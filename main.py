11a08701d9584b904ceed73888c0c83a8ffff416
Selim Poladov <gegerout06@gmail.com>
2025-01-02 16:09:28 +0300

    upd: comments added, project description added

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 def generate_truth_tables(input_n):
+    """
+    Генерирует все возможные таблицы истинности для указанного числа переменных
+
+    :param input_n: Число переменных в логической функции
+    :return: Генератор, возвращающий таблицы истинности
+    """
     num_tables = 1 << (1 << input_n)
     for i in range(num_tables):
         truth_table = []
@@ -8,14 +14,32 @@ def generate_truth_tables(input_n):
 
 
 def is_t0(truth_table):
+    """
+    Проверяет, является ли функция сохраняющей 0
+
+    :param truth_table: Таблица истинности функции
+    :return: True, если функция T0, иначе False
+    """
     return truth_table[0] == 0
 
 
 def is_t1(truth_table):
+    """
+    Проверяет, является ли функция сохраняющей 1
+
+    :param truth_table: Таблица истинности функции
+    :return: True, если функция T1, иначе False
+    """
     return truth_table[-1] == 1
 
 
 def is_self_dual(truth_table):
+    """
+    Проверяет, является ли функция самодвойственной
+
+    :param truth_table: Таблица истинности функции
+    :return: True, если функция самодвойственная, иначе False
+    """
     size = len(truth_table)
     for i in range(size // 2):
         if truth_table[i] == truth_table[-i - 1]:
@@ -24,6 +48,13 @@ def is_self_dual(truth_table):
 
 
 def is_monotone(truth_table, input_n):
+    """
+    Проверяет, является ли функция монотонной
+
+    :param truth_table: Таблица истинности функции
+    :param input_n: Число переменных в функции
+    :return: True, если функция монотонная, иначе False
+    """
     size = 1 << input_n
     for i in range(size):
         for j in range(size):
@@ -34,6 +65,12 @@ def is_monotone(truth_table, input_n):
 
 
 def walsh_transform(f):
+    """
+    Применяет преобразование Уолша к набору значений функции
+
+    :param f: Список значений функции
+    :return: Преобразованный список
+    """
     size = len(f)
     result = f[:]
     step = 1
@@ -49,11 +86,23 @@ def walsh_transform(f):
 
 
 def is_linear(truth_table):
+    """
+    Проверяет, является ли функция линейной
+
+    :param truth_table: Таблица истинности функции
+    :return: True, если функция линейная, иначе False
+    """
     walsh = walsh_transform([(-1) ** x for x in truth_table])
     return all(abs(x) == len(truth_table) or x == 0 for x in walsh)
 
 
 def classify_post_classes(input_n):
+    """
+   Подсчитывает количество всех булевых функций для каждой конфигурации классов Поста
+
+    :param input_n: Число переменных в логической функции
+    :return: Словарь, где ключи — это кортежи с классификацией, а значения — количество функций в этом классе
+    """
     cur_classifications = {}
 
     for truth_table in generate_truth_tables(input_n):
